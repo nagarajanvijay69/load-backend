4cfaa5b0224a12028d921991a2fbbf4cd98307af
nagarajanvijay69 <nagarajanvijay69@gmail.com>
2025-05-08 20:54:23 +0530

    j

--- index.js
+++ index.js
@@ -68,5 +68,5 @@ app.post('/api/login', (req, res) => {
   }
 });
 
-const PORT =  5000;
+const PORT =  "https://load-backend-pmfq.onrender.com" || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
